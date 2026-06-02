c3c293e50129a3bb29fc9be311752344b7f65431
Dmytro <dimadraganchuk8@gmail.com>
2026-06-02 22:34:53 +0100

    Configure Prisma 6 and create services API

---
+++ src/app/api/services/route.ts
@@ -0,0 +1,17 @@
+import { prisma } from "../../../lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const services = await prisma.service.findMany();
+
+    return NextResponse.json(services);
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json(
+      { error: "Failed to fetch services" },
+      { status: 500 }
+    );
+  }
+}
